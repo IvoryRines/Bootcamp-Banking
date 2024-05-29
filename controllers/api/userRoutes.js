92f9b1c224a813c8003b52e421fc90e24a1aea99
Swemee <sweme1@gmail.com>
2024-05-28 20:29:00 -0600

    Added Account Registering

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,24 +2,20 @@ const router = require('express').Router();
 const User = require('../../models/User');
 
 // CREATE new user
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
     const dbUserData = await User.create({
       username: req.body.username,
       email: req.body.email,
       password: req.body.password,
     });
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      req.session.user = dbUserData;
-
-      res.status(200).json(dbUserData);
-    });
+    
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
+  res.redirect("/login");
+
 });
 
 // Login

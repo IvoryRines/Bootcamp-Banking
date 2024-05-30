7eacf4b7f1e88af6d5d5a91630776dcc0bd9fc71
EyeVorE <subplot_05tracer@icloud.com>
2024-05-30 16:42:07 -0500

    fixed a few errors

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,35 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Checking, Savings } = require("../models");
 const withAuth = require("../utils/auth");
 router.get("/", withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ["password"] },
+    //   order: [["name", "ASC"]],
+    // });
+    // const users = userData.map((project) => project.get({ plain: true }));
+
+    // use sequelize and pull the data for the checking & savings where the userId mataches what is in session
+
+    const checkingData = await Checking.findOne({
+      where: {
+        user_id: req.session.user_id,
+      },
     });
-    const users = userData.map((project) => project.get({ plain: true }));
+
+    const checking = checkingData.get({ plain: true });
+
+    const savingsData = await Savings.findOne({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    const savings = savingsData.get({ plain: true });
+
     res.render("homepage", {
-      users,
+      savings,
+      checking,
       logged_in: req.session.logged_in,
     });
   } catch (err) {

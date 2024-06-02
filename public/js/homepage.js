f764bff68b7c176e6d19b13d8050bc80c37184dd
EyeVorE <subplot_05tracer@icloud.com>
2024-06-02 06:07:50 -0500

    made a lot of minor edits for styling and functionality

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,50 +1,49 @@
 const openCheckingHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // TODO: make accountNumber a random 9 digit number generator
-    const accountNumber = 1;
-    const accountBalance = 0;
+  // TODO: make accountNumber a random 9 digit number generator
+  const accountNumber = 1111112;
+  const accountBalance = 0;
 
-    const response = await fetch('/api/accounts/checking', {
-        method: 'POST',
-        body: JSON.stringify({ accountNumber, accountBalance }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-        
-    if (response.ok) {
-        document.location.replace('/checking');
-        alert('Checking account opened!');
-    } else {
-        alert('Failed to open account. You may have already created one.');
-        document.location.replace('/checking');
-    }
+  const response = await fetch("/api/accounts/checking", {
+    method: "POST",
+    body: JSON.stringify({ accountNumber, accountBalance }),
+    headers: { "Content-Type": "application/json" },
+  });
 
+  if (response.ok) {
+    document.location.replace("/checking");
+    alert("Checking account opened!");
+  } else {
+    alert("Failed to open account. You may have already created one.");
+    document.location.replace("/checking");
+  }
 };
 
 const openSavingsHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const accountNumber = 2;
-    const accountBalance = 0;
+  const accountNumber = 2222223;
+  const accountBalance = 0;
 
-    const response = await fetch('/api/accounts/savings', {
-        method: 'POST',
-        body: JSON.stringify({ accountNumber, accountBalance }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/savings');
-        alert('Savings account opened!');
-      } else {
-        alert('Failed to open account. You may have already created one');
-        document.location.replace('/savings');
-      }
+  const response = await fetch("/api/accounts/savings", {
+    method: "POST",
+    body: JSON.stringify({ accountNumber, accountBalance }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/savings");
+    alert("Savings account opened!");
+  } else {
+    alert("Failed to open account. You may have already created one");
+    document.location.replace("/savings");
+  }
 };
 
 document
-  .querySelector('#checking')
-  .addEventListener('click', openCheckingHandler);
+  .querySelector("#checking")
+  .addEventListener("click", openCheckingHandler);
 document
-  .querySelector('#savings')
-  .addEventListener('click', openSavingsHandler);
+  .querySelector("#savings")
+  .addEventListener("click", openSavingsHandler);

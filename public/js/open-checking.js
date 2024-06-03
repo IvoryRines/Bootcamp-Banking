7047444e387dc7529fbab6d720f2f98f796351ac
Paul Sherrill <pauldsherrill@yahoo.com>
2024-06-03 18:26:45 -0500

    removed unnecessary handlebars files and made navbar functional for every conditional

--- public/js/open-checking.js
+++ public/js/open-checking.js
@@ -22,34 +22,7 @@ const openCheckingHandler = async (event) => {
       }
   };
   
-  const openSavingsHandler = async (event) => {
-    event.preventDefault();
-  
-      var minm = 100000000;
-      var maxm = 999999999;
-      const accountNumber = Math.floor(Math .random() * (maxm - minm + 1)) + minm; 
-  
-      const accountBalance = 0;
-  
-    const response = await fetch("/api/accounts/savings", {
-      method: "POST",
-      body: JSON.stringify({ accountNumber, accountBalance }),
-      headers: { "Content-Type": "application/json" },
-    });
-  
-    if (response.ok) {
-      document.location.replace("/savings");
-      alert("Savings account opened!");
-    } else {
-      alert("Failed to open account. You may have already created one");
-      document.location.replace("/savings");
-    }
-  };
   
   document
     .querySelector("#open-checking")
     .addEventListener("click", openCheckingHandler);
-  document
-    .querySelector("#open-savings")
-    .addEventListener("click", openSavingsHandler);
-  
---
+++ public/js/open-savings.js
@@ -0,0 +1,27 @@
+const openSavingsHandler = async (event) => {
+    event.preventDefault();
+  
+      var minm = 100000000;
+      var maxm = 999999999;
+      const accountNumber = Math.floor(Math .random() * (maxm - minm + 1)) + minm; 
+  
+      const accountBalance = 0;
+  
+    const response = await fetch("/api/accounts/savings", {
+      method: "POST",
+      body: JSON.stringify({ accountNumber, accountBalance }),
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/savings");
+      alert("Savings account opened!");
+    } else {
+      alert("Failed to open account. You may have already created one");
+      document.location.replace("/savings");
+    }
+  };
+
+  document
+    .querySelector("#open-savings")
+    .addEventListener("click", openSavingsHandler);

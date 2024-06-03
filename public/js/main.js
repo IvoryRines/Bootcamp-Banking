704540d5523fb00d6fd52c24456db5311b7a69f9
Paul Sherrill <pauldsherrill@yahoo.com>
2024-06-03 00:18:43 -0500

    merged files into one homepage

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+const openCheckingHandler = async (event) => {
+    event.preventDefault();
+  
+      var minm = 100000000;
+      var maxm = 999999999;
+      const accountNumber = Math.floor(Math .random() * (maxm - minm + 1)) + minm; 
+  
+      const accountBalance = 0;
+  
+      const response = await fetch('/api/accounts/checking', {
+          method: 'POST',
+          body: JSON.stringify({ accountNumber, accountBalance }),
+          headers: { 'Content-Type': 'application/json' },
+      });
+          
+      if (response.ok) {
+          document.location.replace('/checking');
+          alert('Checking account opened!');
+      } else {
+          alert('Failed to open account. You may have already created one.');
+          document.location.replace('/checking');
+      }
+  };
+  
+  const openSavingsHandler = async (event) => {
+    event.preventDefault();
+  
+      var minm = 100000000;
+      var maxm = 999999999;
+      const accountNumber = Math.floor(Math .random() * (maxm - minm + 1)) + minm; 
+  
+      const accountBalance = 0;
+  
+    const response = await fetch("/api/accounts/savings", {
+      method: "POST",
+      body: JSON.stringify({ accountNumber, accountBalance }),
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/savings");
+      alert("Savings account opened!");
+    } else {
+      alert("Failed to open account. You may have already created one");
+      document.location.replace("/savings");
+    }
+  };
+  
+  document
+    .querySelector("#open-checking")
+    .addEventListener("click", openCheckingHandler);
+  document
+    .querySelector("#open-savings")
+    .addEventListener("click", openSavingsHandler);
+  

cd61ebb92a2a423d8e6f627d0be1a485d2d56854
EyeVorE <subplot_05tracer@icloud.com>
2024-06-03 21:27:34 -0500

    remove console.logs and fixed withdrawal buttons

--- public/js/checking.js
+++ public/js/checking.js
@@ -4,15 +4,12 @@ const transferInputHandler = async (event) => {
   const transfer = document.querySelector(".transfer-input").value.trim();
   const balance = document.querySelector(".span").textContent.trim();
 
-  console.log(balance);
-  
   function extractNumber(currencyString) {
     // Remove any non-numeric characters
-    return parseFloat(currencyString.replace(/[^0-9.-]+/g,""));
+    return parseFloat(currencyString.replace(/[^0-9.-]+/g, ""));
   }
 
   var number = extractNumber(balance);
-  console.log(number);
 
   let newBalance = number - Number(transfer);
 
@@ -26,12 +23,15 @@ const transferInputHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/checking");
 
-      transferCurrency = Number(transfer).toLocaleString('en-US', {minimumFractionDigits: 2})
+      transferCurrency = Number(transfer).toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+      });
 
       alert(`$${transferCurrency} transfer to savings successful!`);
-      localStorage.setItem('lastTransactionStatus', 'success');
+      localStorage.setItem("lastTransactionStatus", "success");
     } else {
-      document.querySelector('.transaction').innerHTML = `Last transaction failed`;
+      document.querySelector(".transaction").innerHTML =
+        `Last transaction failed`;
       newBalance = newBalance + Number(transfer);
 
       const revertResponse = await fetch("/api/accounts/checking", {
@@ -42,7 +42,7 @@ const transferInputHandler = async (event) => {
 
       if (revertResponse.ok) {
         alert("Failed to transfer money");
-        localStorage.setItem('lastTransactionStatus', 'transfer-failed');;
+        localStorage.setItem("lastTransactionStatus", "transfer-failed");
       } else {
         alert("Failed to revert transfer");
       }
@@ -58,18 +58,14 @@ const depositInputHandler = async (event) => {
   const deposit = document.querySelector(".deposit-input").value.trim();
   const balance = document.querySelector(".span").textContent.trim();
 
-  console.log(balance);
-  
   function extractNumber(currencyString) {
     // Remove any non-numeric characters
-    return parseFloat(currencyString.replace(/[^0-9.-]+/g,""));
+    return parseFloat(currencyString.replace(/[^0-9.-]+/g, ""));
   }
 
   var number = extractNumber(balance);
-  console.log(number);
 
   const newBalance = number + Number(deposit);
-
 
   if (deposit) {
     const response = await fetch("/api/accounts/checking", {
@@ -81,13 +77,15 @@ const depositInputHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/checking");
 
-      depositCurrency = Number(deposit).toLocaleString('en-US', {minimumFractionDigits: 2})
+      depositCurrency = Number(deposit).toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+      });
 
       alert(`$${depositCurrency} deposit succesful!`);
-      localStorage.setItem('lastTransactionStatus', 'success');
+      localStorage.setItem("lastTransactionStatus", "success");
     } else {
-      document.querySelector('.transaction').innerHTML = `Last deposit failed`;
-      localStorage.setItem('lastTransactionStatus', 'deposit-failed');
+      document.querySelector(".transaction").innerHTML = `Last deposit failed`;
+      localStorage.setItem("lastTransactionStatus", "deposit-failed");
       alert("Failed to deposit money");
     }
   }
@@ -96,18 +94,15 @@ const depositInputHandler = async (event) => {
 const withdrawInputHandler = async (event) => {
   event.preventDefault();
 
-  const withdraw = document.querySelector(".withdraw-input").value.trim();
+  const withdraw = document.querySelector(".withdrawal-input").value.trim();
   const balance = document.querySelector(".span").textContent.trim();
 
-  console.log(balance);
-  
   function extractNumber(currencyString) {
     // Remove any non-numeric characters
-    return parseFloat(currencyString.replace(/[^0-9.-]+/g,""));
+    return parseFloat(currencyString.replace(/[^0-9.-]+/g, ""));
   }
 
   var number = extractNumber(balance);
-  console.log(number);
 
   const newBalance = number - Number(withdraw);
 
@@ -121,71 +116,77 @@ const withdrawInputHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/checking");
 
-      withdrawCurrency = Number(withdraw).toLocaleString('en-US', {minimumFractionDigits: 2})
+      withdrawCurrency = Number(withdraw).toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+      });
 
       alert(`$${withdrawCurrency} withdrawal succesful!`);
-      localStorage.setItem('lastTransactionStatus', 'success');
+      localStorage.setItem("lastTransactionStatus", "success");
     } else {
-      document.querySelector('.transaction').innerHTML = `Last withdrawal failed`;
-      localStorage.setItem('lastTransactionStatus', 'withdrawal-failed');
+      document.querySelector(".transaction").innerHTML =
+        `Last withdrawal failed`;
+      localStorage.setItem("lastTransactionStatus", "withdrawal-failed");
       alert("Failed to withdraw money");
     }
   }
 };
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   // Check last transaction status from local storage
-  const lastTransactionStatus = localStorage.getItem('lastTransactionStatus');
+  const lastTransactionStatus = localStorage.getItem("lastTransactionStatus");
 
   // If the last transaction was successful, update the UI accordingly
-  if (lastTransactionStatus === 'success') {
+  if (lastTransactionStatus === "success") {
     transactionDateHandler();
-  } else if (lastTransactionStatus === 'transfer-failed') {
-    document.querySelector('.transaction').innerHTML = 'Last transfer failed';
-  } else if (lastTransactionStatus === 'deposit-failed') {
-    document.querySelector('.transaction').innerHTML = 'Last deposit failed';
-  } else if (lastTransactionStatus === 'withdrawal-failed') {
-    document.querySelector('.transaction').innerHTML = 'Last withdrawal failed';
+  } else if (lastTransactionStatus === "transfer-failed") {
+    document.querySelector(".transaction").innerHTML = "Last transfer failed";
+  } else if (lastTransactionStatus === "deposit-failed") {
+    document.querySelector(".transaction").innerHTML = "Last deposit failed";
+  } else if (lastTransactionStatus === "withdrawal-failed") {
+    document.querySelector(".transaction").innerHTML = "Last withdrawal failed";
   }
 });
 
 const transactionDateHandler = () => {
-  let updatedAt = document.querySelector('.update').textContent.trim();
-  let updateTime = dayjs(updatedAt).format('h:mm A');
-  let updateDate = dayjs(updatedAt).format('MMMM D, YYYY');
-  let today = dayjs().format('MMMM D, YYYY');
+  let updatedAt = document.querySelector(".update").textContent.trim();
+  let updateTime = dayjs(updatedAt).format("h:mm A");
+  let updateDate = dayjs(updatedAt).format("MMMM D, YYYY");
+  let today = dayjs().format("MMMM D, YYYY");
   let todayDate = dayjs();
-  let difference = todayDate.diff(updateDate, 'days');
-    
+  let difference = todayDate.diff(updateDate, "days");
+
   if (updateDate == today) {
-    document.querySelector('.update').innerHTML = `${updateTime} today`
+    document.querySelector(".update").innerHTML = `${updateTime} today`;
   } else if (difference == 1) {
-    document.querySelector('.update').innerHTML = `${updateTime} yesterday`
+    document.querySelector(".update").innerHTML = `${updateTime} yesterday`;
   } else {
-    document.querySelector('.update').innerHTML = `${updateTime} on ${updateDate}`
+    document.querySelector(".update").innerHTML =
+      `${updateTime} on ${updateDate}`;
   }
-}
+};
 
 const openedDateHandler = () => {
-  let updatedAt = document.querySelector('.update').textContent.trim();
-  let createdAt = document.querySelector('.create').textContent.trim();
+  let updatedAt = document.querySelector(".update").textContent.trim();
+  let createdAt = document.querySelector(".create").textContent.trim();
   let createTime = dayjs(createdAt);
-  console.log(createTime);
-  document.querySelector('.create').innerHTML = createTime.format('MMMM D, YYYY');
+  document.querySelector(".create").innerHTML =
+    createTime.format("MMMM D, YYYY");
 
   if (updatedAt == createdAt) {
-    document.querySelector('.transaction').innerHTML = ``;
+    document.querySelector(".transaction").innerHTML = ``;
   }
-}
+};
 
 const currencyHandler = () => {
   const balance = document.querySelector(".span").textContent.trim();
 
   const balanceNumber = Number(balance);
 
-  const formattedBalance = balanceNumber.toLocaleString('en-US', {minimumFractionDigits: 2});
+  const formattedBalance = balanceNumber.toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+  });
 
-  document.querySelector('.span').innerHTML = formattedBalance;
+  document.querySelector(".span").innerHTML = formattedBalance;
 };
 
 openedDateHandler();
@@ -198,5 +199,5 @@ document
   .querySelector(".deposit-btn")
   .addEventListener("click", depositInputHandler);
 document
-  .querySelector(".withdraw-btn")
+  .querySelector(".withdrawal-btn")
   .addEventListener("click", withdrawInputHandler);
